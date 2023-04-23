41a88edb9bb5749f61f4d1bfe97ef8c33f90881d
quang1409thanh <thanhaxt342@gmail.com>
2023-04-23 23:43:29 +0700

    sửa lại chế độ cho co giãn màn hình

--- js/login.js
+++ js/login.js
@@ -31,23 +31,86 @@ registerForm.addEventListener('submit', (event) => {
   window.location.href = 'index.html'; // Chuyển hướng đến trang đăng nhập
 });
 
-loginForm.addEventListener('submit', (event) => {
+
+//modal for mobile
+const loginButton_ = document.getElementById('login-button_');
+const registerButton_ = document.getElementById('register-button_');
+const loginForm_ = document.getElementById('login-form_');
+const registerForm_ = document.getElementById('register-form_');
+
+registerButton_.addEventListener('click', () => {
+  loginForm_.style.display = 'none';
+  registerForm_.style.display = 'block';
+  registerButton_.classList.add('active');
+  loginButton_.classList.remove('active');
+});
+
+loginButton_.addEventListener('click', () => {
+  loginForm_.style.display = 'block';
+  registerForm_.style.display = 'none';
+  loginButton_.classList.add('active');
+  registerButton_.classList.remove('active');
+});
+
+registerForm_.addEventListener('submit', (event) => {
   event.preventDefault(); // Ngăn chặn trang web được tải lại sau khi submit form
-  const username = document.getElementById('login-username').value;
-  const password = document.getElementById('login-password').value;
+  const username_ = document.getElementById('register-username_').value;
+  const password_ = document.getElementById('register-password_').value;
+
+  // Lưu thông tin tài khoản đăng ký vào localStorage
+  localStorage.setItem('username_', username_);
+  localStorage.setItem('password_', password_);
+
+  // Thông báo đăng ký thành công và chuyển hướng đến trang đăng nhập
+  alert('Đăng ký thành công!, Bạn hãy đăng nhập !!!');
+  window.location.href = 'index.html'; // Chuyển hướng đến trang đăng nhập
+});
+
+loginForm_.addEventListener('submit', (event) => {
+  event.preventDefault(); // Ngăn chặn trang web được tải lại sau khi submit form
+  const username_= document.getElementById('login-username_').value;
+  const password_ = document.getElementById('login-password_').value;
+  localStorage.setItem('current_login', username_);
 
   // Kiểm tra thông tin đăng nhập
   const savedUsername = localStorage.getItem('username');
   const savedPassword = localStorage.getItem('password');
-
-  if (username === savedUsername && password === savedPassword) {
-    // alert('Đăng nhập thành công!');
+  
+  const savedUsername_ = localStorage.getItem('username_');
+  const savedPassword_ = localStorage.getItem('password_');
+  
+  if ((username_ === savedUsername && password_ === savedPassword) ||(username_ === savedUsername_ && password_ === savedPassword_) ) {
+    // Thực hiện hành động nếu thông tin đăng nhập giống nhau\
     window.location.href = 'uindex.html'; // Chuyển hướng đến trang chính
+
   } else {
     alert('Tên đăng nhập hoặc mật khẩu không chính xác. Vui lòng kiểm tra lại!');
   }
 });
 
+///login
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Ngăn chặn trang web được tải lại sau khi submit form
+  const username = document.getElementById('login-username').value;
+  const password = document.getElementById('login-password').value;
+  localStorage.setItem('current_login', username);
+
+  const savedUsername = localStorage.getItem('username');
+  const savedPassword = localStorage.getItem('password');
+  
+  const savedUsername_ = localStorage.getItem('username_');
+  const savedPassword_ = localStorage.getItem('password_');
+  
+  if ((username === savedUsername && password === savedPassword) ||(username === savedUsername_ && password === savedPassword_) ) {
+    // Thực hiện hành động nếu thông tin đăng nhập giống nhau
+    window.location.href = 'uindex.html'; // Chuyển hướng đến trang chính
+
+  } else {
+    alert('Tên đăng nhập hoặc mật khẩu không chính xác. Vui lòng kiểm tra lại!');
+  }
+});
+// end modal for mobile
 
 // // nạp tiền
 const atm = document.getElementById('atm');
